4a5673343de24f15ae66c53427d471231131e898
Sai Krishna Annam <deepakaanm143@gmail.com>
2025-03-15 11:32:56 +0530

    feature(pdf-flask): updated the config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     CORS_RESOURCES = {
-        '/remove_password': {'origins': 'https://angular-pdf-pr-master.onrender.com'}
+        '/remove_password': {'origins': ['https://angular-pdf-pr-master.onrender.com', 'https://pdf-password-remover-angular.onrender.com']}
     }
